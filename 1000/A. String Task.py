e7f9a3eea7f5fabbe81c3485794981c440e95c03
yogi <yogiahmed12@gmail.com>
2024-08-16 09:20:04 +0300

    2 problem 1000 rate

---
+++ 1000/A. String Task.py
@@ -0,0 +1,35 @@
+class Solution:
+    def __init__(self) -> None:
+        """
+        Initialize the Solution class and define the vowels to be removed.
+        """
+        self.vowels = ("a", "e", "i", "o", "u", "y")
+    
+    def string_task(self, word: str) -> str:
+        """
+        Process the input string by:
+        1. Converting all characters to lowercase.
+        2. Removing all vowels (a, e, i, o, u, y).
+        3. Adding a '.' before each consonant.
+
+        Parameters:
+        word (str): The input string consisting of uppercase and lowercase Latin letters.
+
+        Returns:
+        str: The processed string after removing vowels, adding a '.', and converting to lowercase.
+        """
+        result = ""
+        for letter in word.lower():
+            if letter in self.vowels:
+                pass
+            else:
+                result += f".{letter}"
+        return result    
+
+def main():
+    word: str = input("Enter a string: ")
+    result: str = Solution().string_task(word)
+    print(result)
+    
+if __name__ == '__main__':
+    main()
---
+++ 1000/A. Theatre Square.py
@@ -0,0 +1,23 @@
+from math import ceil
+class Solution:
+    def cover ( a_length : int,a_width : int, b_length : int)-> int:
+        """
+        Calculate the minimum number of flagstones required to cover a rectangular area.
+
+        Parameters:
+        a_length (int): The length of the rectangular area (n).
+        a_width (int): The width of the rectangular area (m).
+
+        Returns:
+            int: number of flagstones needs.
+        """
+        return ceil((a_length*a_width)/(b_length*b_length))
+
+def main():
+    
+    inputs :list[int] = list(map(int,input().split(" ")))
+    result :int = Solution.cover(*inputs)
+    print(result)
+    
+if __name__ == '__main__':
+    main()
